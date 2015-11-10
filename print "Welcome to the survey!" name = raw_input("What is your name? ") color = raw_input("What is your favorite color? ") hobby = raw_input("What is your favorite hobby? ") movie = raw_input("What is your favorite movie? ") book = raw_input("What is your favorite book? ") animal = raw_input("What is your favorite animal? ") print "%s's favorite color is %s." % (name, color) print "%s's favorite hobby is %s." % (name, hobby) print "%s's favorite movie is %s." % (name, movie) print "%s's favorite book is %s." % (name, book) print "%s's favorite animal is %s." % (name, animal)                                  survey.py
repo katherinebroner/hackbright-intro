7c7e34b11ec5ad22eddf5188d151361d8001b469
katherinebroner <katherine.broner@gmail.com>
2015-11-09 19:55:00 -0800

    Create print "Welcome to the survey!" name = raw_input("What is your name? ") color = raw_input("What is your favorite color? ") hobby = raw_input("What is your favorite hobby? ") movie = raw_input("What is your favorite movie? ") book = raw_input("What is your favorite book? ") animal = raw_input("What is your favorite animal? ") print "%s's favorite color is %s." % (name, color) print "%s's favorite hobby is %s." % (name, hobby) print "%s's favorite movie is %s." % (name, movie) print "%s's favorite book is %s." % (name, book) print "%s's favorite animal is %s." % (name, animal)                                  survey.py

---
+++ print "Welcome to the survey!" name = raw_input("What is your name? ") color = raw_input("What is your favorite color? ") hobby = raw_input("What is your favorite hobby? ") movie = raw_input("What is your favorite movie? ") book = raw_input("What is your favorite book? ") animal = raw_input("What is your favorite animal? ") print "%s's favorite color is %s." % (name, color) print "%s's favorite hobby is %s." % (name, hobby) print "%s's favorite movie is %s." % (name, movie) print "%s's favorite book is %s." % (name, book) print "%s's favorite animal is %s." % (name, animal)                                  survey.py
@@ -0,0 +1,12 @@
+print "Welcome to the survey!"
+name = raw_input("What is your name? ")
+color = raw_input("What is your favorite color? ")
+hobby = raw_input("What is your favorite hobby? ")
+movie = raw_input("What is your favorite movie? ")
+book = raw_input("What is your favorite book? ")
+animal = raw_input("What is your favorite animal? ")
+print "%s's favorite color is %s." % (name, color)
+print "%s's favorite hobby is %s." % (name, hobby)
+print "%s's favorite movie is %s." % (name, movie)
+print "%s's favorite book is %s." % (name, book)
+print "%s's favorite animal is %s." % (name, animal)                                             
