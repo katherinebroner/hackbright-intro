2cd35410b21b8873852d9acbbfad53a09cf0db75
Katherine Broner <katherine.broner@gmail.com>
2015-11-12 19:04:14 -0800

    11/12 updates fun_conditionals1

--- fun_conditionals1.py
+++ fun_conditionals1.py
@@ -6,9 +6,8 @@ def check_for_string (input):
 check_for_string (8)
 
 def larger (num1, num2, num3):
-	if num1 > num2:
-		if num1 > num3:
-			print num1
+	if num1 > num2 and num1 > num3:
+		print num1
 	elif num2 > num3: 
 			print num2
 	else:
@@ -17,9 +16,8 @@ def larger (num1, num2, num3):
 larger (4,63,32)
 
 def smaller (num1, num2, num3):
-	if num1 < num2:
-		if num1 < num3:
-			print num1
+	if num1 < num2 and num1 < num3:
+		print num1
 	elif num2 < num3:
 			print num2
 	else: 
